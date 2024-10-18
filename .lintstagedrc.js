addc62e70c95b5d1ab0a9cd9ecbe3bfb096bb50b
Junior Silva <zezinhojun@gmail.com>
2024-10-18 15:26:34 -0300

    first commit com prettier, husky

---
+++ .lintstagedrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx}": (filenames) => [
+    `prettier --write ${filenames.map((f) => `"${f}"`).join(" ")}`,
+    `npm run lint --fix . ${filenames.map((f) => `--file "${f}"`).join(" ")}`,
+  ],
+};
