cd6743b1beb462ffb77d6b4d25b73c5edd2d475f
Junior Silva <zezinhojun@gmail.com>
2024-10-19 13:41:00 -0300

    testes no lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,6 @@ module.exports = {
   "*.{js,jsx,ts,tsx}": (filenames) => [
     `prettier --write ${filenames.map((f) => `"${f}"`).join(" ")}`,
     `npm run lint --fix . ${filenames.map((f) => `--file "${f}"`).join(" ")}`,
+    `npm test -- --findRelatedTests ${filenames.join(" ")}`,
   ],
 };
--- src/app/components/Main/test.tsx
+++ src/app/components/Main/test.tsx
@@ -1,12 +1,17 @@
 import { screen, render } from "@testing-library/react";
-import Main from "@/app/components/Main";
-import "@testing-library/jest-dom";
+import Main from ".";
 
 describe("<Main />", () => {
   it("Should render the heading", () => {
-    render(<Main />);
+    //renderiza o componente
+    const { container } = render(<Main />);
+
+    //busca o elemento e verifica a existência dele
     expect(
       screen.getByRole("heading", { name: /react avançado/i }),
     ).toBeInTheDocument();
+
+    //gerar snapshot
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
