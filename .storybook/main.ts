e7f03fdb7db00dd40dc6dcd31551c6ca75be1144
Junior Silva <zezinhojun@gmail.com>
2025-09-23 23:41:33 -0300

    fix(storybook): optimize storybook bundle size

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,7 +12,50 @@ const config: StorybookConfig = {
 
   webpackFinal: async (config) => {
     config.resolve?.modules?.push(`${process.cwd()}/src`, "node_modules");
+
+    if (config.optimization) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+            priority: 10,
+          },
+          react: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: "react",
+            chunks: "all",
+            priority: 20,
+          },
+          storybook: {
+            test: /[\\/]node_modules[\\/]@storybook[\\/]/,
+            name: "storybook",
+            chunks: "all",
+            priority: 30,
+          },
+        },
+      };
+    }
+
+    if (config.performance) {
+      config.performance.hints = false;
+      config.performance.maxAssetSize = 512000;
+      config.performance.maxEntrypointSize = 512000;
+    }
+
     return config;
   },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
 };
+
 export default config;
